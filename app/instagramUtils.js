d342225f95cfa540cb18adeb526397f32e2b0b5e
Jules Moretti <julesmoretti@me.com>
2015-01-02 12:23:08 -0800

    tracing error

--- app/instagramUtils.js
+++ app/instagramUtils.js
@@ -1500,13 +1500,10 @@ var specialCounter = 0;
               console.log("API error or access_token missing or oauth_limit rate reached so stopped account "+fancrawl_instagram_id+" on server restart");
               console.log("STOPPED STATE: ", fancrawl_instagram_id);
             });
-    if ( fancrawl_instagram_id === 571377691 || fancrawl_instagram_id === "571377691" ) {
-      console.log("||||||||| +++ startIndividual failed validity test" );
-    }
           } else {
 
     if ( fancrawl_instagram_id === 571377691 || fancrawl_instagram_id === "571377691" ) {
-      console.log("||||||||| +++ startIndividual passed validity test" );
+      console.log("||||||||| +++ startIndividual passed validity test", rows );
     }
             // IF USER WAS STARTED
             if ( rows[0].state && rows[0].state === "started" ) {
